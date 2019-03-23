b7697444f97ca542c710cf6da6a43a6d6a1009a7
Adam <adam.peltenburg@gmail.com>
2019-03-23 15:20:09 -0400

    Created main menu GUI form.

---
+++ DurakGame/DurakGame_Forms/Durak.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurakGame_Forms
+{
+    class Durak
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ DurakGame/DurakGame_Forms/DurakForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DurakGame_Client
+{
+    public partial class frmDurak : Form
+    {
+        public frmDurak()
+        {
+            InitializeComponent();
+        }
+    }
+}
