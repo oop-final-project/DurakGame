d26c66651259797b9b1916bd87f1ce9d3905ac71
Purab jayeshkumar Barot <purabjayeshkumar.barot@dcmail.ca>
2019-04-07 15:24:12 -0400

    docked the button

--- DurakGame/DurakForms/DurakGame.Designer.cs
+++ DurakGame/DurakForms/DurakGame.Designer.cs
@@ -41,8 +41,8 @@
             this.btnEndAttack = new System.Windows.Forms.Button();
             this.btnEndDefence = new System.Windows.Forms.Button();
             this.DurakTip = new System.Windows.Forms.ToolTip(this.components);
-            this.gbxAdder = new System.Windows.Forms.GroupBox();
             this.pnlAdder = new System.Windows.Forms.Panel();
+            this.gbxAdder = new System.Windows.Forms.GroupBox();
             this.gbxComputer.SuspendLayout();
             this.gbxHuman.SuspendLayout();
             this.gbxRiver.SuspendLayout();
@@ -52,10 +52,9 @@
             // btnMainMenu
             // 
             this.btnMainMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMainMenu.Location = new System.Drawing.Point(4, 15);
-            this.btnMainMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMainMenu.Location = new System.Drawing.Point(3, 12);
             this.btnMainMenu.Name = "btnMainMenu";
-            this.btnMainMenu.Size = new System.Drawing.Size(156, 52);
+            this.btnMainMenu.Size = new System.Drawing.Size(117, 42);
             this.btnMainMenu.TabIndex = 1;
             this.btnMainMenu.Text = "&Main Menu";
             this.DurakTip.SetToolTip(this.btnMainMenu, "click to Go back to main menu");
@@ -72,21 +71,18 @@
             this.gbxComputer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.gbxComputer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbxComputer.ForeColor = System.Drawing.Color.Aqua;
-            this.gbxComputer.Location = new System.Drawing.Point(227, 15);
-            this.gbxComputer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxComputer.Location = new System.Drawing.Point(170, 12);
             this.gbxComputer.Name = "gbxComputer";
-            this.gbxComputer.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gbxComputer.Size = new System.Drawing.Size(855, 177);
+            this.gbxComputer.Size = new System.Drawing.Size(642, 144);
             this.gbxComputer.TabIndex = 0;
             this.gbxComputer.TabStop = false;
             this.gbxComputer.Text = "Defender";
             // 
             // pnlCPU
             // 
-            this.pnlCPU.Location = new System.Drawing.Point(1, 23);
-            this.pnlCPU.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlCPU.Location = new System.Drawing.Point(1, 19);
             this.pnlCPU.Name = "pnlCPU";
-            this.pnlCPU.Size = new System.Drawing.Size(853, 150);
+            this.pnlCPU.Size = new System.Drawing.Size(640, 122);
             this.pnlCPU.TabIndex = 0;
             this.DurakTip.SetToolTip(this.pnlCPU, "Cards For Defender");
             // 
@@ -100,21 +96,18 @@
             this.gbxHuman.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.gbxHuman.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbxHuman.ForeColor = System.Drawing.Color.Cyan;
-            this.gbxHuman.Location = new System.Drawing.Point(227, 383);
-            this.gbxHuman.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxHuman.Location = new System.Drawing.Point(170, 311);
             this.gbxHuman.Name = "gbxHuman";
-            this.gbxHuman.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gbxHuman.Size = new System.Drawing.Size(855, 178);
+            this.gbxHuman.Size = new System.Drawing.Size(640, 144);
             this.gbxHuman.TabIndex = 0;
             this.gbxHuman.TabStop = false;
             this.gbxHuman.Text = "Attacker";
             // 
             // pnlHuman
             // 
-            this.pnlHuman.Location = new System.Drawing.Point(1, 26);
-            this.pnlHuman.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlHuman.Location = new System.Drawing.Point(1, 21);
             this.pnlHuman.Name = "pnlHuman";
-            this.pnlHuman.Size = new System.Drawing.Size(853, 150);
+            this.pnlHuman.Size = new System.Drawing.Size(641, 122);
             this.pnlHuman.TabIndex = 1;
             this.DurakTip.SetToolTip(this.pnlHuman, "Cards for attcker");
             // 
@@ -122,9 +115,10 @@
             // 
             this.lblTalonCount.AutoSize = true;
             this.lblTalonCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTalonCount.Location = new System.Drawing.Point(40, 185);
+            this.lblTalonCount.Location = new System.Drawing.Point(30, 150);
+            this.lblTalonCount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTalonCount.Name = "lblTalonCount";
-            this.lblTalonCount.Size = new System.Drawing.Size(46, 31);
+            this.lblTalonCount.Size = new System.Drawing.Size(38, 26);
             this.lblTalonCount.TabIndex = 9;
             this.lblTalonCount.Text = "99";
             this.DurakTip.SetToolTip(this.lblTalonCount, "Cards left in deck");
@@ -139,21 +133,18 @@
             this.gbxRiver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.gbxRiver.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbxRiver.ForeColor = System.Drawing.Color.Aqua;
-            this.gbxRiver.Location = new System.Drawing.Point(228, 198);
-            this.gbxRiver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbxRiver.Location = new System.Drawing.Point(171, 161);
             this.gbxRiver.Name = "gbxRiver";
-            this.gbxRiver.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gbxRiver.Size = new System.Drawing.Size(855, 177);
+            this.gbxRiver.Size = new System.Drawing.Size(641, 144);
             this.gbxRiver.TabIndex = 10;
             this.gbxRiver.TabStop = false;
             this.gbxRiver.Text = "River";
             // 
             // pnlRiver
             // 
-            this.pnlRiver.Location = new System.Drawing.Point(1, 23);
-            this.pnlRiver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlRiver.Location = new System.Drawing.Point(1, 19);
             this.pnlRiver.Name = "pnlRiver";
-            this.pnlRiver.Size = new System.Drawing.Size(853, 150);
+            this.pnlRiver.Size = new System.Drawing.Size(640, 122);
             this.pnlRiver.TabIndex = 0;
             this.DurakTip.SetToolTip(this.pnlRiver, "Cards played");
             // 
@@ -161,10 +152,10 @@
             // 
             this.btnEndAttack.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnEndAttack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEndAttack.Location = new System.Drawing.Point(5, 509);
-            this.btnEndAttack.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnEndAttack.Location = new System.Drawing.Point(3, 414);
+            this.btnEndAttack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEndAttack.Name = "btnEndAttack";
-            this.btnEndAttack.Size = new System.Drawing.Size(208, 58);
+            this.btnEndAttack.Size = new System.Drawing.Size(156, 47);
             this.btnEndAttack.TabIndex = 11;
             this.btnEndAttack.Text = "&End Attack";
             this.btnEndAttack.UseVisualStyleBackColor = true;
@@ -174,15 +165,23 @@
             // 
             this.btnEndDefence.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnEndDefence.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEndDefence.Location = new System.Drawing.Point(5, 508);
-            this.btnEndDefence.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnEndDefence.Location = new System.Drawing.Point(3, 414);
+            this.btnEndDefence.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEndDefence.Name = "btnEndDefence";
-            this.btnEndDefence.Size = new System.Drawing.Size(208, 58);
+            this.btnEndDefence.Size = new System.Drawing.Size(156, 47);
             this.btnEndDefence.TabIndex = 12;
             this.btnEndDefence.Text = "&End Defence";
             this.btnEndDefence.UseVisualStyleBackColor = true;
             this.btnEndDefence.Visible = false;
             this.btnEndDefence.Click += new System.EventHandler(this.btnEndDefence_Click);
+            // 
+            // pnlAdder
+            // 
+            this.pnlAdder.Location = new System.Drawing.Point(0, 19);
+            this.pnlAdder.Name = "pnlAdder";
+            this.pnlAdder.Size = new System.Drawing.Size(242, 122);
+            this.pnlAdder.TabIndex = 0;
+            this.DurakTip.SetToolTip(this.pnlAdder, "Cards played");
             // 
             // gbxAdder
             // 
@@ -194,32 +193,21 @@
             this.gbxAdder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.gbxAdder.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbxAdder.ForeColor = System.Drawing.Color.Aqua;
-            this.gbxAdder.Location = new System.Drawing.Point(1091, 198);
-            this.gbxAdder.Margin = new System.Windows.Forms.Padding(4);
+            this.gbxAdder.Location = new System.Drawing.Point(818, 161);
             this.gbxAdder.Name = "gbxAdder";
-            this.gbxAdder.Padding = new System.Windows.Forms.Padding(4);
-            this.gbxAdder.Size = new System.Drawing.Size(323, 177);
+            this.gbxAdder.Size = new System.Drawing.Size(246, 144);
             this.gbxAdder.TabIndex = 13;
             this.gbxAdder.TabStop = false;
             this.gbxAdder.Text = "Added Cards";
             this.gbxAdder.Visible = false;
             // 
-            // pnlAdder
-            // 
-            this.pnlAdder.Location = new System.Drawing.Point(0, 23);
-            this.pnlAdder.Margin = new System.Windows.Forms.Padding(4);
-            this.pnlAdder.Name = "pnlAdder";
-            this.pnlAdder.Size = new System.Drawing.Size(323, 150);
-            this.pnlAdder.TabIndex = 0;
-            this.DurakTip.SetToolTip(this.pnlAdder, "Cards played");
-            // 
             // frmDurakGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::DurakForms.Properties.Resources.TableTop;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1423, 710);
+            this.ClientSize = new System.Drawing.Size(1076, 577);
             this.Controls.Add(this.gbxAdder);
             this.Controls.Add(this.btnEndDefence);
             this.Controls.Add(this.btnEndAttack);
@@ -229,7 +217,6 @@
             this.Controls.Add(this.gbxComputer);
             this.Controls.Add(this.btnMainMenu);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "frmDurakGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
